7a863513fe8e7e77e83a0544cb7b5087d619c864
Bradley BARBIER <bradley.barbier@outlook.fr>
2023-03-29 12:09:50 +0200

    Create formatPagination()

--- apies/api_tedyspo-service/src/services/utils/FormatterAPI.php
+++ apies/api_tedyspo-service/src/services/utils/FormatterAPI.php
@@ -4,6 +4,7 @@ namespace atelier\tedyspo\services\utils;
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Routing\RouteContext;
 
 class FormatterAPI
 {
@@ -15,5 +16,30 @@ class FormatterAPI
     $rs->getBody()->write(json_encode($data));
 
     return $rs;
+  }
+
+  public static function formatPagination(Request $rq, string $nameRoute, int $actualPagination, array $parameters, int $count, int $size): array
+  {
+    $routeParser = RouteContext::fromRequest($rq)->getRouteParser();
+
+    $data = [
+      'count' => $count,
+      'links' => [
+        'self' => $routeParser->urlFor($nameRoute, [], array_merge($parameters, ['page' => $actualPagination, 'size' => $size])),
+        'first' => $routeParser->urlFor($nameRoute, [], array_merge($parameters, ['page' => 1, 'size' => $size])),
+        'last' => $routeParser->urlFor($nameRoute, [], array_merge($parameters, ['page' => (ceil($count / $size) > 0 ? ceil($count / $size) : 1), 'size' => $size])),
+      ],
+    ];
+
+    if ($actualPagination > 1) {
+      $data['links']['prev'] = $routeParser->urlFor($nameRoute, [], array_merge($parameters, ['page' => $actualPagination - 1, 'size' => $size]));
+    }
+
+    if ($actualPagination < ceil($count / $size)) {
+      $data['links']['next'] = $routeParser->urlFor($nameRoute, [], array_merge($parameters, ['page' => $actualPagination + 1, 'size' => $size]));
+    }
+
+
+    return $data;
   }
 }
