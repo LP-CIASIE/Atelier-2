982aca0a96ed7d6f0c1f86cd70d47bc38b2101cb
lecyp12 <contact@cyprien-cotinaut.com>
2023-03-29 16:22:29 +0200

    correctif cors

--- apies/api_gateway-service/public/index.php
+++ apies/api_gateway-service/public/index.php
@@ -44,10 +44,8 @@ $app->options('/{routes:.+}', function ($request, $response, $args) {
 $app->add(function ($request, $handler) {
   $response = $handler->handle($request);
   return $response
-    ->withHeader('Access-Control-Allow-Origin', '*')
-    ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-    ->withHeader('Access-Control-Allow-Credentials', 'true')
-    ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    ->withHeader('Access-Control-Allow-Origin', $request->getHeader('Origin'))
+    ->withHeader('Access-Control-Allow-Headers', 'Content-Type');
 });
 /**
  * API Basic Route
