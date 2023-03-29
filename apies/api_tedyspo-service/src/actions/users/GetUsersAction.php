08217406568d032a27a2120a2f036b0c8946a403
Bradley BARBIER <bradley.barbier@outlook.fr>
2023-03-29 12:12:19 +0200

    Create /users route api

--- apies/api_tedyspo-service/src/actions/users/GetUsersAction.php
+++ apies/api_tedyspo-service/src/actions/users/GetUsersAction.php
@@ -3,13 +3,28 @@
 namespace atelier\tedyspo\actions\users;
 
 use atelier\tedyspo\actions\AbstractAction;
+use atelier\tedyspo\services\utils\FormatterAPI;
 
 class GetUsersAction extends AbstractAction
 {
   public function __invoke($request, $response, $args)
   {
+    $params = $request->getQueryParams();
+
+    $page = $params['page'] ?? 1;
+    $size = $params['size'] ?? 10;
+
+    $parameters = $request->getQueryParams();
+
     $userService = $this->container->get('service.user');
 
-    return $response;
+    $users = $userService->getUsers($parameters);
+    $count = $userService->getCount();
+
+
+    $data = FormatterAPI::formatPagination($request, 'get_users', $page, $parameters, $count, $size);
+    $data['users'] = $users;
+
+    return FormatterAPI::formatResponse($request, $response, $data, 200);
   }
 }
