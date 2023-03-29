d9d799b3ed4f6dff093269a9332756fe3f710d26
Bradley BARBIER <bradley.barbier@outlook.fr>
2023-03-29 09:38:24 +0200

    Install PrimeVue (#3)

--- front/src/main.js
+++ front/src/main.js
@@ -4,9 +4,15 @@ import App from "./App.vue";
 import { createPinia } from "pinia";
 import router from "./router";
 
+// PrimeVue
+import PrimeVue from "primevue/config";
+import "primevue/resources/themes/md-light-indigo/theme.css";
+import "primevue/resources/primevue.min.css";
+
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(PrimeVue);
 
 app.mount("#app");
