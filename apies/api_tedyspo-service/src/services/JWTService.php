205b0c756e089c9790b65520f4222fdb098ec25d
Bradley BARBIER <bradley.barbier@outlook.fr>
2023-03-29 12:14:34 +0200

    Create a decodeDataOfJWT()

--- apies/api_tedyspo-service/src/services/JWTService.php
+++ apies/api_tedyspo-service/src/services/JWTService.php
@@ -2,13 +2,33 @@
 
 namespace atelier\tedyspo\services;
 
+use Respect\Validation\Validator as v;
+
 class JWTService extends AbstractService
 {
-  public function decodeDataOfJWT($jwt)
+  public function decodeDataOfJWT($Authorization)
   {
+    $jwt = $Authorization[0] ?? null;
+
+    try {
+      v::stringType()->notEmpty()->validate($jwt);
+    } catch (\Exception $e) {
+      throw new \Exception('Token manquant', 400);
+    }
+
     $jwt = explode('.', $jwt);
+
+    if (count($jwt) !== 3) {
+      throw new \Exception('Token invalide', 400);
+    }
+
     $jwt = $jwt[1];
     $jwt = base64_decode($jwt);
+
+    if ($jwt === false) {
+      throw new \Exception('Token invalide', 400);
+    }
+
     $jwt = json_decode($jwt, true);
     return $jwt;
   }
