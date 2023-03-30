b2159bf6204dac508cd0820b88c3923b2670981e
Bradley BARBIER <bradley.barbier@outlook.fr>
2023-03-30 16:30:24 +0200

    Create basic action | TEST WAITING EVENT ACTION

--- apies/api_tedyspo-service/src/actions/comments/CreateCommentAction.php
+++ apies/api_tedyspo-service/src/actions/comments/CreateCommentAction.php
@@ -3,6 +3,8 @@
 namespace atelier\tedyspo\actions\comments;
 
 use atelier\tedyspo\actions\AbstractAction;
+use atelier\tedyspo\services\utils\FormatterAPI;
+use atelier\tedyspo\services\utils\FormatterObject;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
@@ -11,12 +13,15 @@ class CreateCommentAction extends AbstractAction
   public function __invoke(Request $request, Response $response, $args)
   {
     $user = $this->parseJWT($request);
-
     $data = $this->parseBody($request);
 
     $commentService = $this->container->get('service.comment');
     $comment = $commentService->create($data, $user['uid'], $args['id_event']);
 
-    return $response;
+    $data = [
+      'comment' => FormatterObject::Comment($comment)
+    ];
+
+    return FormatterAPI::formatResponse($request, $response, $data, 201);
   }
 }
