5bda671ccb242fb40ad27eb3d6ecec03073e48f8
Bradley BARBIER <bradley.barbier@outlook.fr>
2023-03-30 09:49:15 +0200

    Add route for create Comment

--- apies/api_tedyspo-service/src/services/CommentService.php
+++ apies/api_tedyspo-service/src/services/CommentService.php
@@ -2,6 +2,46 @@
 
 namespace atelier\tedyspo\services;
 
+use atelier\tedyspo\models\Comment;
+use atelier\tedyspo\models\Event;
+use atelier\tedyspo\models\User;
+use Respect\Validation\Validator as v;
+
 class CommentService extends AbstractService
 {
+  public function createComment($data, $id_user, $id_event)
+  {
+    try {
+      v::key('comment', v::stringType()->notEmpty())
+        ->assert($data);
+    } catch (\Exception $e) {
+      throw new \Exception('Données invalides', 400);
+    }
+
+    try {
+      User::findOrFail($id_user);
+    } catch (\Exception $e) {
+      throw new \Exception('Identifiant utilisateur invalide', 400);
+    }
+
+    try {
+      Event::findOrFail($id_event);
+    } catch (\Exception $e) {
+      throw new \Exception('Identifiant événement invalide', 400);
+    }
+
+    $comment = new Comment();
+    $comment->id_comment = \Ramsey\Uuid\Uuid::uuid4();
+    $comment->comment = $data['comment'];
+    $comment->id_user = $id_user;
+    $comment->id_event = $id_event;
+
+    try {
+      $comment->save();
+    } catch (\Exception $e) {
+      throw new \Exception('Erreur lors de la création du commentaire', 500);
+    }
+
+    return $comment;
+  }
 }
