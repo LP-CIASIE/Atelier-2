977b1eed3d944b9ded5b58b52c7f36783c06726d
Bradley BARBIER <bradley.barbier@outlook.fr>
2023-03-30 12:25:24 +0200

    Generate new SQL Script

---
+++ apies/api_auth-service/sql/user_schema.sql
@@ -0,0 +1,20 @@
+-- Adminer 4.8.1 MySQL 5.5.5-10.11.2-MariaDB-1:10.11.2+maria~ubu2204 dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id_user` varchar(36) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` varchar(20) NOT NULL,
+  `refresh_token` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_user`),
+  UNIQUE KEY `mail_UNIQUE` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+
+-- 2023-03-30 10:23:00
