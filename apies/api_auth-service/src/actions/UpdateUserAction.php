52dd5d22c64fc86ced2a8051bb1c2554e63b1b9f
Bradley BARBIER <bradley.barbier@outlook.fr>
2023-03-30 08:56:50 +0200

    [FIX] Bad import

--- apies/api_auth-service/src/actions/UpdateUserAction.php
+++ apies/api_auth-service/src/actions/UpdateUserAction.php
@@ -4,12 +4,11 @@ namespace atelier\auth\actions\users;
 
 use atelier\auth\services\TokenService;
 use atelier\auth\services\UserService;
-use atelier\tedyspo\actions\AbstractAction;
-use atelier\tedyspo\services\utils\FormatterAPI;
+use atelier\auth\services\utils\FormatterAPI;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
-class UpdateUserAction extends AbstractAction
+class UpdateUserAction
 {
   public function __invoke(Request $request, Response $response, $args)
   {
