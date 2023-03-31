364fc0f8a4e1953fcb7e76e36bd5d77d357fb6ce
Teddy CLEMENT DELS <teddy.clement_dels@outlook.fr>
2023-03-31 15:14:01 +0200

    ajout du post

--- apies/api_tedyspo-service/src/actions/locations/CreateLocationAction.php
+++ apies/api_tedyspo-service/src/actions/locations/CreateLocationAction.php
@@ -3,6 +3,7 @@
 namespace atelier\tedyspo\actions\locations;
 
 use atelier\tedyspo\actions\AbstractAction;
+use atelier\tedyspo\services\utils\FormatterAPI;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
@@ -12,6 +13,11 @@ class CreateLocationAction extends AbstractAction
   {
     $locationService = $this->container->get('service.location');
 
-    return $response;
+    $data = $request->getParsedBody();
+    $params = $request->getQueryParams();
+    $id_event = $params['id_event'];
+    $locationService->createLocation($data,$id_event);
+    FormatterAPI::formatResponse($request, $response, $data, 201); 
+
   }
 }
--- apies/api_tedyspo-service/src/services/LocationService.php
+++ apies/api_tedyspo-service/src/services/LocationService.php
@@ -1,7 +1,54 @@
 <?php
 
 namespace atelier\tedyspo\services;
+use atelier\tedyspo\models\Location;
+use Ramsey\Uuid\Uuid;
+use Respect\Validation\Validator as v;
 
 class LocationService extends AbstractService
 {
+    final public function createLocation($data,$id_event)
+    {
+        $location = new Location();
+        $location->id_location = Uuid::uuid4();
+            
+        try{
+
+        v::stringType()->length(3, 100)->assert($data['name']);
+        }
+        catch (\Exception $e){
+            throw new \Exception("Erreur lors de la création du titre", 400);
+        }
+        $location->name = $data['name'];
+             
+        try{
+            v::decimals(2)->validate($data['lat']);
+        }catch (\Exception $e){
+            throw new \Exception("Erreur lors de la création de la latitude", 400);
+        }
+        $location->lat = $data['lat'];
+        
+
+        try{
+            v::decimals(2)->validate($data['long']);
+
+        }catch (\Exception $e){
+            throw new \Exception("Erreur lors de la création de la date", 400);
+        }
+        $location->long = $data['long'];
+        try{
+            v::between(0, 1)->validate($data['is_related']);
+        }
+        catch (\Exception $e){
+            throw new \Exception("Erreur lors de la création de la visibilité", 400);
+        }
+        $location->is_related = $data['is_related'];
+        try{
+            v::uuid()->validate($data['id_event']);
+        }
+        catch (\Exception $e){
+            throw new \Exception("Erreur lors de la création de l'event", 400);
+        }
+        $location->id_event = $id_event;
+    }
 }
