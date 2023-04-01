fef990e4dfa3296cd7020778bb19fc69b9d363ae
Teddy CLEMENT DELS <teddy.clement_dels@outlook.fr>
2023-04-01 21:15:00 +0200

    path abstractAction

--- apies/api_gateway-service/src/actions/AbstractAction.php
+++ apies/api_gateway-service/src/actions/AbstractAction.php
@@ -31,6 +31,7 @@ abstract class AbstractAction
       $client = $this->container->get('client.auth.service');
     }
 
+
     try {
       if ($method === "post") {
         $responseHTTP = $client->post($route, [
@@ -41,25 +42,16 @@ abstract class AbstractAction
           ],
           'body' => json_encode($request->getParsedBody()),
         ]);
-      } catch (\GuzzleHttp\Exception\RequestException $e) {
-        $response = new ResponseSlim();
-        $jsonDecode = json_decode($e->getResponse()->getBody()->getContents(), true);
-        // var_dump($jsonDecode);
-        // die();
-        $exceptionData = [
-          'code' => $jsonDecode['exception'][0]['code'] ?? 500,
-          'message' => $jsonDecode['exception'][0]['message'] ?? 'Erreur inconnue',
-        ];
-
-        $jsonExceptionData = json_encode($exceptionData, JSON_UNESCAPED_UNICODE);
-
-        $response->getBody()->write($jsonExceptionData);
-        $response = $response->withHeader('Content-Type', 'application/json');
-        return $response->withStatus($exceptionData['code']);
-      }
-    } else if ($route === '/signin') {
-      try {
-        $responseHTTP = $clientAuth->post($route, [
+      } else if ($method === "get") {
+        $responseHTTP = $client->get($route, [
+          'query' => $request->getQueryParams(),
+          'headers' => [
+            'Authorization' => $token,
+            'Content-Type' => $this->container->get('content.type')
+          ],
+        ]);
+      } else if ($method === "put") {
+        $responseHTTP = $client->put($route, [
           'query' => $request->getQueryParams(),
           'headers' => [
             'Authorization' => $token,
@@ -67,25 +59,8 @@ abstract class AbstractAction
           ],
           'body' => json_encode($request->getParsedBody()),
         ]);
-      } catch (\GuzzleHttp\Exception\RequestException $e) {
-        $response = new ResponseSlim();
-        $jsonDecode = json_decode($e->getResponse()->getBody()->getContents(), true);
-        // var_dump($jsonDecode);
-        // die();
-        $exceptionData = [
-          'code' => $jsonDecode['exception'][0]['code'] ?? 500,
-          'message' => $jsonDecode['exception'][0]['message'] ?? 'Erreur inconnue',
-        ];
-
-        $jsonExceptionData = json_encode($exceptionData, JSON_UNESCAPED_UNICODE);
-
-        $response->getBody()->write($jsonExceptionData);
-        $response = $response->withHeader('Content-Type', 'application/json');
-        return $response->withStatus($exceptionData['code']);
-      }
-    } else if ($route === '/users' && $method === 'put') {
-      try {
-        $responseHTTP = $clientAuth->put($route, [
+      } else if ($method === "delete") {
+        $responseHTTP = $client->delete($route, [
           'query' => $request->getQueryParams(),
           'headers' => [
             'Authorization' => $token,
@@ -93,89 +68,34 @@ abstract class AbstractAction
           ],
           'body' => json_encode($request->getParsedBody()),
         ]);
-        $responseHTTP = $clientTedyspo->put($route, [
-          'query' => $request->getQueryParams(),
-          'headers' => [
-            'Authorization' => $token,
-            'Content-Type' => $this->container->get('content.type')
-          ],
-          'body' => json_encode($request->getParsedBody()),
-        ]);
-      } catch (\GuzzleHttp\Exception\RequestException $e) {
-        $response = new ResponseSlim();
-        $jsonDecode = json_decode($e->getResponse()->getBody()->getContents(), true);
-        // var_dump($jsonDecode);
-        // die();
-        $exceptionData = [
-          'code' => $jsonDecode['exception'][0]['code'] ?? 500,
-          'message' => $jsonDecode['exception'][0]['message'] ?? 'Erreur inconnue',
-        ];
-
-        $jsonExceptionData = json_encode($exceptionData, JSON_UNESCAPED_UNICODE);
-
-        $response->getBody()->write($jsonExceptionData);
-        $response = $response->withHeader('Content-Type', 'application/json');
-        return $response->withStatus($exceptionData['code']);
       }
-    } else {
-      try {
-        if ($method === "post") {
-          $responseHTTP = $clientTedyspo->post($route, [
-            'query' => $request->getQueryParams(),
-            'headers' => [
-              'Authorization' => $token,
-              'Content-Type' => $this->container->get('content.type')
-            ],
-            'body' => json_encode($request->getParsedBody()),
-          ]);
-        } else if ($method === "get") {
-          $responseHTTP = $clientTedyspo->get($route, [
-            'query' => $request->getQueryParams(),
-            'headers' => [
-              'Authorization' => $token,
-              'Content-Type' => $this->container->get('content.type')
-            ],
-          ]);
-        } else if ($method === "put") {
-          $responseHTTP = $clientTedyspo->put($route, [
-            'query' => $request->getQueryParams(),
-            'headers' => [
-              'Authorization' => $token,
-              'Content-Type' => $this->container->get('content.type')
-            ],
-            'body' => json_encode($request->getParsedBody()),
-          ]);
-        } else if ($method === "delete") {
-          $responseHTTP = $clientTedyspo->delete($route, [
-            'query' => $request->getQueryParams(),
-            'headers' => [
-              'Authorization' => $token,
-              'Content-Type' => $this->container->get('content.type')
-            ],
-            'body' => json_encode($request->getParsedBody()),
-          ]);
-        }
-      } catch (\GuzzleHttp\Exception\RequestException $e) {
-        $response = new ResponseSlim();
-        $jsonDecode = json_decode($e->getResponse()->getBody()->getContents(), true);
-        // var_dump($jsonDecode);
-        // die();
-        $exceptionData = [
-          'code' => $jsonDecode['exception'][0]['code'] ?? 500,
-          'message' => $jsonDecode['exception'][0]['message'] ?? 'Erreur inconnue',
-        ];
+    } catch (\GuzzleHttp\Exception\RequestException $e) {
+      $response = new ResponseSlim();
+      $jsonDecode = json_decode($e->getResponse()->getBody()->getContents(), true);
+      // var_dump($jsonDecode);
+      // die();
+      $code = $jsonDecode['exception'][0]['code'] ?? 500;
+      $code = $code === 0 ? 500 : $code;
 
-        $jsonExceptionData = json_encode($exceptionData, JSON_UNESCAPED_UNICODE);
+      $exceptionData = [
+        'code' => $code,
+        'message' => $jsonDecode['exception'][0]['message'] ?? 'Erreur inconnue',
+      ];
 
-        $response->getBody()->write($jsonExceptionData);
-        $response = $response->withHeader('Content-Type', 'application/json');
-        return $response->withStatus($exceptionData['code']);
-      }
+      $jsonExceptionData = json_encode($exceptionData, JSON_UNESCAPED_UNICODE);
+
+      $response->getBody()->write($jsonExceptionData);
+      $response = $response->withHeader('Content-Type', 'application/json');
+      return $response->withStatus($exceptionData['code']);
     }
+
 
     $logger = $this->container->get('logger');
     $logger->info("{$method} | {$this->container->get('gateway.atelier.local')}{$route} | {$responseHTTP->getStatusCode()}");
 
-    return $responseHTTP;
+    $response = new ResponseSlim();
+    $response->getBody()->write($responseHTTP->getBody()->getContents());
+    $response = $response->withHeader('Content-Type', 'application/json');
+    return $response->withStatus($responseHTTP->getStatusCode());
   }
 }
