3bb1484b6c66ba6938d0a41378bddafeae2c7182
ugo zanzi <ugo.zanzi@gmail.com>
2023-04-03 10:07:45 +0200

    delete User in auth api

--- apies/api_gateway-service/src/actions/users/DeleteUserAction.php
+++ apies/api_gateway-service/src/actions/users/DeleteUserAction.php
@@ -10,6 +10,7 @@ final class DeleteUserAction extends AbstractAction
 {
   public function __invoke(Request $request, Response $response, $args)
   {
-    return $this->sendRequest($request, $response, '/users' . '/' . $args['id_user'], 'delete', 'tedyspo');
+    $this->sendRequest($request, $response, '/users' . '/' . $args['id_user'], 'delete', 'auth');
+    $this->sendRequest($request, $response, '/users' . '/' . $args['id_user'], 'delete', 'tedyspo');
   }
 }
