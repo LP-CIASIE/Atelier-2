29f9fdf003fcf6bf1320a28184eafd6bd0216744
Christopher JUE <78968379+JUEChristopher@users.noreply.github.com>
2023-04-03 18:15:15 +0200

    Working on invitation card

--- flutter/lib/screen/home_screen.dart
+++ flutter/lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lp1_ciasie_atelier_2/screen/events/waiting_list_screen.dart';
 import 'package:lp1_ciasie_atelier_2/screen/profil/profil_builder_screen.dart';
 import 'package:lp1_ciasie_atelier_2/screen/auth/sign_in_screen.dart';
 import 'package:lp1_ciasie_atelier_2/screen/auth/sign_up_screen.dart';
@@ -14,39 +15,47 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
-      body: Column(children: [
-        ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const SignInPage()),
-            );
-          },
-          child: const Text("Se connecter"),
+        appBar: AppBar(
+          title: const Text('Home'),
         ),
-        ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const SignUpPage()),
-            );
-          },
-          child: const Text("S'inscrire"),
-        ),
-        ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => const ProfilBuilderPage()),
-            );
-          },
-          child: const Text("Mon Profil"),
-        ),
-      ]),
-    );
+        body: Column(children: [
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const SignInPage()),
+              );
+            },
+            child: const Text("Se connecter"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const SignUpPage()),
+              );
+            },
+            child: const Text("S'inscrire"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const ProfilBuilderPage()),
+              );
+            },
+            child: const Text("Mon Profil"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const WaitingList()),
+              );
+            },
+            child: const Text("Invitations en attente"),
+          ),
+        ]));
   }
 }
