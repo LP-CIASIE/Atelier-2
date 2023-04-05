012d4a24ec1ccbfe978f0b1de2a07a40cfcf15b3
Bradley BARBIER <bradley.barbier@outlook.fr>
2023-04-05 23:05:38 +0200

    Set online API

--- front/src/App.vue
+++ front/src/App.vue
@@ -9,7 +9,7 @@ import mitt from "mitt";
 
 // Variable Globale pour Axios nommé "api"
 const API = axios.create({
-	baseURL: "http://gateway.atelier.local:8000",
+	baseURL: "https://api.tedyspo.cyprien-cotinaut.com",
 	headers: {
 		"Content-Type": "application/json",
 	},
